37bb27ba4fe36bfd4afe193c658c279f6a33cbdc
sehoong0429 <tjsgh0801@naver.com>
2022-05-09 10:13:51 +0900

    Initial Commit

---
+++ DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Seoyeon Hong on 2022/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
